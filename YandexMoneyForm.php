7451dd088ce90587562d8a8229ce2732e01b1d65
/usr/bin/nano <support@beget.ru>
2017-04-06 01:43:09 +0300

    deleted:    AutoloadExample.php 	new file:   Module.php 	modified:   README.md 	new file:   YandexMoneyForm.php

---
+++ YandexMoneyForm.php
@@ -0,0 +1,50 @@
+<?php
+
+namespace Baykov\yandexform;
+
+/**
+ * This is just an example.
+ */
+class YandexMoneyForm extends \yii\base\Widget
+{
+    public function run()
+    {
+        return '<iframe 
+frameborder="0" 
+allowtransparency="true" 
+scrolling="no" 
+src="https://money.yandex.ru/embed/donate.xml?account=410012154592693&amp;
+quickpay=donate&amp;
+payment-type-choice=on&amp;
+default-sum=&amp;
+targets=%D0%A3%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5+%D0%BD%D0%BE%D0%BC%D0%B5%D1%80+%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0&amp;
+target-visibility=on&amp;
+project-name=&amp;
+project-site=http%3A%2F%2Fmagazin-nozhei.com%2F&amp;
+button-text=03&amp;
+comment=on&amp;
+hint=%D0%9F%D0%BE%D0%B4%D1%81%D0%BA%D0%B0%D0%B7%D0%BA%D0%B0%3A+%D0%9E%D0%BF%D0%BB%D0%B0%D1%82%D0%B0+%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0+%E2%84%96&amp;
+fio=on&amp;
+mail=on&amp;
+phone=on&amp;
+successURL=" 
+mce_src="https://money.yandex.ru/embed/donate.xml?account=410012154592693&amp;
+quickpay=donate&amp;
+payment-type-choice=on&amp;
+default-sum=&amp;
+targets=%D0%A3%D0%BA%D0%B0%D0%B6%D0%B8%D1%82%D0%B5+%D0%BD%D0%BE%D0%BC%D0%B5%D1%80+%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0&amp;
+target-visibility=on&amp;
+project-name=&amp;
+project-site=http%3A%2F%2Fmagazin-nozhei.com%2F&amp;
+button-text=03&amp;
+comment=on&amp;
+hint=%D0%9F%D0%BE%D0%B4%D1%81%D0%BA%D0%B0%D0%B7%D0%BA%D0%B0%3A+%D0%9E%D0%BF%D0%BB%D0%B0%D1%82%D0%B0+%D0%B7%D0%B0%D0%BA%D0%B0%D0%B7%D0%B0+%E2%84%96&amp;
+fio=on&amp;
+mail=on&amp;
+phone=on&amp;
+successURL=" 
+width="497" 
+height="176">
+</iframe>';
+    }
+}
